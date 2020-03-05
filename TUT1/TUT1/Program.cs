d1171bba79f7c7a9111c2e8f46ca261e6520c26e
Mateusz Dzierzęcki <s19808@pjwstk.edu.pl>
2020-03-05 16:52:28 +0100

    Emails crawler added

---
+++ TUT1/TUT1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TUT1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var websiteUrl = args[0];
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(websiteUrl);
+            if (response.IsSuccessStatusCode) {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(htmlContent);
+
+                foreach (var emailAddress in matches)
+                {
+                    Console.WriteLine(emailAddress.ToString());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
